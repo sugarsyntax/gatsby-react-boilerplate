1c57627accd6a3d8fce093dd8d7421307b6b039d
sugarsyntax <jacquelline500@gmail.com>
2018-10-12 02:30:15 -0700

    First commit

---
+++ src/components/header/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import './_header.scss';
+
+const Header = () =>
+  (<header className="header justify-content-center">
+    <div className="d-flex">
+      <div className="flex-column">
+        <div className="col-12">
+          <div className="header-content">
+            <span className="header-logo">
+              <span style={{ color:"#fb9999" }}>J</span>
+              <span style={{ color:"#fc9ca7" }}>a</span>
+              <span style={{ color:"#fc9fb4" }}>c</span>
+              <span style={{ color:"#fda2c2" }}>q</span>
+              <span style={{ color:"#b3b177" }}>u</span>
+              <span style={{ color:"#68bf2b" }}>e</span>
+              <span style={{ color:"#59d11d" }}>l</span>
+              <span style={{ color:"#49e40f" }}>l</span>
+              <span style={{ color:"#3af601" }}>i</span>
+              <span style={{ color:"#72eb74" }}>n</span>
+              <span style={{ color:"#aadfe7" }}>e</span>
+            </span>
+            <nav className="header-nav container-fluid col-12">
+              <ul className="header-nav-list">
+                <li className="header-nav-list-item">
+                  <a href="#features">Features</a>
+                </li>
+                <li className="header-nav-list-item">
+                  <a href="#howto">How To</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>);
+
+export default Header;
